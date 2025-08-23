151efb48c7127c8fd3a9799138aaa0220b8ecd2c
David <daviddanielarch@gmail.com>
2025-08-23 08:36:31 -0300

    migrate state to s3

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    railway = {
-      source  = "terraform-community-providers/railway"
-      version = "0.5.2"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "railway" {
   # Configuration options
 }
